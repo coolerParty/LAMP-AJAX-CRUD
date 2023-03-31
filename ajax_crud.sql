46bcb95b6dcd90deb69ace0038b35937cd6d170a
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2023-03-31 12:17:18 +0800

    Change table export Book, To DB ajax_crud.

--- ajax_crud.sql
+++ ajax_crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2023 at 06:03 AM
+-- Generation Time: Mar 31, 2023 at 06:16 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
